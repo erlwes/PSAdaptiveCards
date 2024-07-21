8035783203982237e9750d94756f9deb260669c1
erlwes <88742199+erlwes@users.noreply.github.com>
2024-07-21 22:58:45 +0200

    Update WIP-Invoke-PostJsonToWebhook.ps1

--- WIP-Invoke-PostJsonToWebhook.ps1
+++ WIP-Invoke-PostJsonToWebhook.ps1
@@ -1,8 +1,13 @@
+# Webhook URI will be generated when you add a workflow to your teams-channel and select the template "post-to-a-channel-when-a-webhook-request-is-received"
+# Read more here: https://devblogs.microsoft.com/microsoft365dev/retirement-of-office-365-connectors-within-microsoft-teams/
+# Example URI: 'https://prod-140.westeurope.logic.azure.com:443/workflows/[REDACTED]/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=[REDACTED]'
+
+# THIS FUNTION NOT TESTED WITH PIPELINES AND OUTPUT FROM New-AdaptiveCard YET! SHOULD BE STRAIGHT FORWARD.
+
 function Invoke-PostJsonToWebhook {
     param (
         [Parameter(Mandatory = $true)]
         [string]$WebhookURI,
-
         [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
         [string]$Json
     )
